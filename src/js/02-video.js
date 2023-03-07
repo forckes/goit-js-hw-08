85286fe20594795b684e9b9511e316d001712e77
forckes <forckes@proton.me>
2023-03-07 16:47:10 +0200

    Task 2

---
+++ src/js/02-video.js
@@ -0,0 +1,31 @@
+import "../css/common.css";
+import "../css/02-video.css";
+import Player from "@vimeo/player";
+import throttle from "lodash.throttle";
+
+const iframe = document.querySelector("iframe");
+const player = new Player(iframe);
+const CURRENT_TIME = "videoplayer-current-time";
+
+function timeUpdate(data) {
+	console.log(data.seconds);
+	localStorage.setItem(CURRENT_TIME, data.seconds);
+}
+
+player.on("timeupdate", throttle(timeUpdate, 1000));
+
+const currentTime = JSON.parse(localStorage.getItem(CURRENT_TIME));
+
+player
+	.setCurrentTime(currentTime)
+	.then(function (seconds) {})
+	.catch(function (error) {
+		switch (error.name) {
+			case "RangeError":
+				seconds < 0 || seconds > 571.52;
+				break;
+
+			default:
+				break;
+		}
+	});
